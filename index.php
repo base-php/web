060d9682b267ef495284fc1d07fb3abd8eb6539c
Nisa Delgado <nisadelgado@gmail.com>
2022-09-02 14:20:55 -0400

    Primer commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Base PHP</title>
+
+	<link rel="icon" href="/img/favicon.svg">
+
+	<link rel="stylesheet" href="/node_modules/@fortawesome/fontawesome-free/css/all.css">
+	<link rel="stylesheet" href="/css/style.css">
+
+	<script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+	<div class="text-center">
+		<h1 class="text-6xl font-bold">
+			<i class="fa fa-shapes"></i> 
+			Base PHP
+		</h1>
+
+		<h2 class="text-3xl">Un framework para minimalistas</h2>
+
+		<div class="mt-3">
+			<a target="_blank" href="https://github.com/base-php">
+				<span class="fab fa-twitter"></span>
+			</a>
+
+			<a target="_blank" href="https://youtube.com/base-php">
+				<span class="fab fa-youtube"></span>
+			</a>
+
+			<a target="_blank" href="mailto:info@base-php.com">
+				<span class="fas fa-envelope"></span>
+			</a>
+		</div>
+	</div>
+</body>
+</html>
