79e2c9608c9aa6a4881091b30cfa77cecfa3187a
Nisa Delgado <nisadelgado@gmail.com>
2022-09-04 12:46:15 -0400

    Plantilla para página de documentación

---
+++ docs/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Base PHP</title>
+
+	<link rel="icon" href="/img/favicon.svg">
+
+	<link rel="stylesheet" href="/node_modules/flowbite/src/flowbite.css">
+	<link rel="stylesheet" href="/node_modules/@fortawesome/fontawesome-free/css/all.css">
+	<link rel="stylesheet" href="/css/docs.css">
+
+	<script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+	<div class="w-full">
+		<div class="flex">
+			<div class="w-3/12 sidebar">
+				<h1 class=" ml-8 pl-6 text-2xl mt-3 font-bold">
+					<i class="fa fa-shapes"></i>
+					Base PHP
+				</h1>
+
+				<div class="menu ml-8 mt-8">
+					<div class="ml-3 mt-5">
+						<a href="" class="block m-3">Prólogo</a>
+						<a href="" class="block m-3">Empezando</a>
+						<a href="" class="block m-3">Conceptos de arquitectura</a>
+						<a href="" class="block m-3">Básico</a>
+						<a href="" class="block m-3">Más a fondo</a>
+						<a href="" class="block m-3">Seguridad</a>
+						<a href="" class="block m-3">Base de datos</a>
+						<a href="" class="block m-3">Eloquent ORM</a>
+						<a href="" class="block m-3">Pruebas</a>
+						<a href="" class="block m-3">Paquetes</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="w-9/12 bg-white content">Contenido</div>
+		</div>
+	</div>
+</body>
+</html>
