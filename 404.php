65cdce370ba59d40f5fcb1a80404a6a7f5bc9cf7
Nisa Delgado <nisadelgado@gmail.com>
2022-11-07 08:32:34 -0400

    Error 404

---
+++ 404.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Base PHP</title>
+
+	<link rel="icon" href="/img/favicon.svg">
+
+	<link rel="stylesheet" href="/node_modules/flowbite/src/flowbite.css">
+	<link rel="stylesheet" href="/node_modules/@fortawesome/fontawesome-free/css/all.css">
+	<link rel="stylesheet" href="/css/style.css">
+
+	<script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+	<div class="text-center">
+		<h2 class="text-3xl">404 | Página no encontrada</h2>
+
+		<div class="mt-3">
+			<a href="/">
+				<span class="fa fa-reply"></span> Volver a inicio
+			</a>
+		</div>
+	</div>
+
+	<script src="/node_modules/flowbite/dist/flowbite.js"></script>
+</body>
+</html>
