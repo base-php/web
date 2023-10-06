27cf1668a2d5e86c97b526eb4d7bcf3c247dfbb9
Nisa Delgado <nisadelgado@gmail.com>
2023-10-06 00:06:30 -0400

    Íconos de Github y Whatsapp agregados

--- index.php
+++ index.php
@@ -22,12 +22,20 @@
 		<h2 class="text-3xl">Un framework para minimalistas</h2>
 
 		<div class="mt-3">
-			<a data-tooltip-target="tooltip-twitter" target="_blank" href="https://github.com/base-php">
-				<span class="fab fa-twitter"></span>
+			<a data-tooltip-target="tooltip-github" target="_blank" href="https://github.com/base-php">
+				<span class="fab fa-github"></span>
 			</a>
 
-			<div id="tooltip-twitter" role="tooltip" class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
-			    Ir a Twitter
+			<div id="tooltip-github" role="tooltip" class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
+			    Ir a Github
+			</div>
+
+			<a data-tooltip-target="tooltip-x" target="_blank" href="https://twitter.com/base-php">
+				<span class="fab fa-x-twitter"></span>
+			</a>
+
+			<div id="tooltip-x" role="tooltip" class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
+			    Ir a X
 			</div>
 
 
@@ -46,6 +54,14 @@
 
 			<div id="tooltip-mail" role="tooltip" class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
 			    Enviar un correo electrónico
+			</div>
+
+			<a data-tooltip-target="tooltip-docs" target="_blank" href="https://wa.me/123456789">
+				<span class="fab fa-whatsapp"></span>
+			</a>
+
+			<div id="tooltip-docs" role="tooltip" class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
+			    Ir a la Whatsapp
 			</div>
 
 
